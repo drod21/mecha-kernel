230b32674e518040c3401f41d4dd0d46ccac861c
Derek Rodriguez <drod2169@gmail.com>
2011-03-29 18:21:50 -0400

    OC thanks to Cyanogenmod and my config files

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -53,11 +53,14 @@
 
 #define MAX_AXI_KHZ 192000
 
+#define PLL2_L_VAL_ADDR  (MSM_CLK_CTL_BASE + 0x33c)
+
 struct clock_state {
 	struct clkctl_acpu_speed	*current_speed;
 	struct mutex			lock;
 	uint32_t			acpu_switch_time_us;
 	uint32_t			vdd_switch_time_us;
+	unsigned long                   power_collapse_khz;
 	unsigned long			wait_for_irq_khz;
 	int				wfi_ramp_down;
 	int				pwrc_ramp_down;
@@ -80,26 +83,78 @@ static struct cpufreq_frequency_table freq_table[] = {
 	{ 0, 245760 },
 	{ 1, 368640 },
 	{ 2, 768000 },
+#if defined(CONFIG_MACH_SPADE) || defined(CONFIG_MACH_GLACIER)
+	{ 3, 1017600 },
+	{ 4, 1113600 },
+	{ 5, 1209600 },
+	{ 6, 1305600 },
+	{ 7, 1401600 },
+	{ 8, 1497600 },
+	{ 9, 1516800 },
+#ifndef CONFIG_JESUS_PHONE
+	{ 10, CPUFREQ_TABLE_END },
+#else
+	/* Just an example of some of the insanity I was able to pull off on my
+	   device */
+	{ 10, 1612800 },
+	{ 11, 1708800 },
+	{ 12, 1804800 },
+	{ 13, CPUFREQ_TABLE_END },
+#endif
+#else
 	{ 3, 806400 },
-	{ 4, CPUFREQ_TABLE_END },
+	{ 4, 1017600 },
+	{ 5, 1113600 },
+	{ 6, 1209600 },
+	{ 7, 1305600 },
+	{ 8, 1401600 },
+	{ 9, 1497600 },
+	{ 10, 1516800 },
+#ifndef CONFIG_JESUS_PHONE
+	{ 11, CPUFREQ_TABLE_END },
+#else
+	/* Just an example of some of the insanity I was able to pull off on my
+	   device */
+	{ 11, 1612800 },
+	{ 12, 1708800 },
+	{ 13, 1804800 },
+	{ 14, CPUFREQ_TABLE_END },
+#endif
+#endif
 };
 
 /* Use negative numbers for sources that can't be enabled/disabled */
 #define SRC_LPXO (-2)
 #define SRC_AXI  (-1)
 static struct clkctl_acpu_speed acpu_freq_tbl[] = {
-	{ 24576,  SRC_LPXO, 0, 0,  30720,  1000, VDD_RAW(1000) },
-	{ 61440,  PLL_3,    5, 11, 61440,  1000, VDD_RAW(1000) },
-	{ 122880, PLL_3,    5, 5,  61440,  1000, VDD_RAW(1000) },
-	{ 184320, PLL_3,    5, 4,  61440,  1000, VDD_RAW(1000) },
-	{ MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440, 1000, VDD_RAW(1000) },
-	{ 245760, PLL_3,    5, 2,  61440,  1000, VDD_RAW(1000) },
-	{ 368640, PLL_3,    5, 1,  122800, 1050, VDD_RAW(1050) },
-	{ 768000, PLL_1,    2, 0,  153600, 1100, VDD_RAW(1100) },
-	/* ACPU >= 806.4MHz requires MSMC1 @ 1.2V. Voting for
-	 * AXI @ 192MHz accomplishes this implicitly. 806.4MHz
-	 * is updated to 1024MHz at runtime for QSD8x55. */
+	{ 24576,  SRC_LPXO, 0, 0,  30720,  900, VDD_RAW(850) },
+	{ 61440,  PLL_3,    5, 11, 61440,  900, VDD_RAW(900) },
+	{ 122880, PLL_3,    5, 5,  61440,  900, VDD_RAW(900) },
+	{ 184320, PLL_3,    5, 4,  61440,  900, VDD_RAW(900) },
+	{ MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440, 900, VDD_RAW(900) },
+	{ 245760, PLL_3,    5, 2,  61440,  900, VDD_RAW(900) },
+	{ 368640, PLL_3,    5, 1,  122800, 900, VDD_RAW(900) },
+	{ 768000, PLL_1,    2, 0,  153600, 1050, VDD_RAW(1050) },
+	/* Make sure any freq based from PLL_2 is a multiple of 19200! 
+	   Voltage tables are being very conservative and are not designed to
+	   be an undervolt of any sort. */
+#if defined(CONFIG_MACH_SPADE) || defined(CONFIG_MACH_GLACIER)
+	{ 1017600, PLL_2,   3, 0,  192000, 1100, VDD_RAW(1100) },
+#else
 	{ 806400, PLL_2,    3, 0,  192000, 1100, VDD_RAW(1100) },
+	{ 1017600, PLL_2,   3, 0,  192000, 1200, VDD_RAW(1200) },
+#endif
+	{ 1113600, PLL_2,   3, 0,  192000, 1200, VDD_RAW(1200) },
+	{ 1209600, PLL_2,   3, 0,  192000, 1200, VDD_RAW(1200) },
+	{ 1305600, PLL_2,   3, 0,  192000, 1200, VDD_RAW(1200) },
+	{ 1401600, PLL_2,   3, 0,  192000, 1300, VDD_RAW(1300) },
+	{ 1497600, PLL_2,   3, 0,  192000, 1300, VDD_RAW(1300) },
+	{ 1516800, PLL_2,   3, 0,  192000, 1300, VDD_RAW(1300) },
+#ifdef CONFIG_JESUS_PHONE
+	{ 1612800, PLL_2,   3, 0,  192000, 1400, VDD_RAW(1400) },
+	{ 1708800, PLL_2,   3, 0,  192000, 1400, VDD_RAW(1400) },
+	{ 1804800, PLL_2,   3, 0,  192000, 1450, VDD_RAW(1450) },
+#endif
 	{ 0 }
 };
 static unsigned long max_axi_rate;
@@ -108,66 +163,33 @@ static unsigned long max_axi_rate;
 unsigned long acpuclk_power_collapse(int from_idle)
 {
 	int ret = acpuclk_get_rate();
-	if (drv_state.pwrc_ramp_down)
-		acpuclk_set_rate(POWER_COLLAPSE_HZ, SETRATE_PC);
-	return ret * 1000;
+	ret *= 1000;
+	if (ret > drv_state.power_collapse_khz)
+		acpuclk_set_rate(drv_state.power_collapse_khz,
+	(from_idle ? SETRATE_PC_IDLE : SETRATE_PC));
+	return ret;
 }
 
 unsigned long acpuclk_get_wfi_rate(void)
 {
-	return drv_state.wait_for_irq_khz * 1000;
+	return drv_state.wait_for_irq_khz;
 }
 
 #define WAIT_FOR_IRQ_HZ (MAX_AXI_KHZ * 1000)
 unsigned long acpuclk_wait_for_irq(void)
 {
 	int ret = acpuclk_get_rate();
-	if (drv_state.wfi_ramp_down)
-		acpuclk_set_rate(WAIT_FOR_IRQ_HZ, SETRATE_SWFI);
-	return ret * 1000;
+	ret *= 1000;
+	if (ret > drv_state.wait_for_irq_khz)
+		acpuclk_set_rate(drv_state.wait_for_irq_khz, SETRATE_SWFI);
+	return ret;
 }
-
-#ifdef CONFIG_HTC_SMEM_MSMC1C2_DEBUG
-/* mARM */
-#define HTC_SMEM_MSMC1		(MSM_SHARED_RAM_BASE + 0x000F8000)
-#define HTC_SMEM_AXI_SPEED	(MSM_SHARED_RAM_BASE + 0x000F8004)
-/* aARM */
-#define HTC_SMEM_MSMC2_STAT	(MSM_SHARED_RAM_BASE + 0x000F8100)
-#define HTC_SMEM_MSMC2_LAST	(MSM_SHARED_RAM_BASE + 0x000F8104)
-#define HTC_SMEM_MSMC2_CURR	(MSM_SHARED_RAM_BASE + 0x000F8108)
-#define HTC_SMEM_MSMC2_MSMC1	(MSM_SHARED_RAM_BASE + 0x000F810C)
-#define HTC_SMEM_MSMC2_AXI	(MSM_SHARED_RAM_BASE + 0x000F8110)
-#endif
 
 static int acpuclk_set_acpu_vdd(struct clkctl_acpu_speed *s)
 {
-#ifdef CONFIG_HTC_SMEM_MSMC1C2_DEBUG
-	unsigned int smem_val;
-	int ret;
-
-	/* store current MSMC1 and AXI */
-	smem_val = readl(HTC_SMEM_MSMC1);
-	writel(smem_val, HTC_SMEM_MSMC2_MSMC1);
-	smem_val = readl(HTC_SMEM_AXI_SPEED);
-	writel(smem_val, HTC_SMEM_MSMC2_AXI);
-
-	/* store last and current(target) MSMC2 */
-	smem_val = readl(HTC_SMEM_MSMC2_CURR);
-	if (smem_val)
-		writel(smem_val, HTC_SMEM_MSMC2_LAST);
-	writel(s->vdd_mv, HTC_SMEM_MSMC2_CURR);
-
-	writel(0x11112222, HTC_SMEM_MSMC2_STAT);
-	ret = msm_spm_set_vdd(s->vdd_raw);
-	if (ret)
-		return ret;
-	/* HTC_SMEM_MSMC2_CURR is valid only when STAT:0x33334444 */
-	writel(0x33334444, HTC_SMEM_MSMC2_STAT);
-#else
 	int ret = msm_spm_set_vdd(s->vdd_raw);
 	if (ret)
 		return ret;
-#endif
 
 	/* Wait for voltage to stabilize. */
 	udelay(drv_state.vdd_switch_time_us);
@@ -190,6 +212,11 @@ static void acpuclk_set_src(const struct clkctl_acpu_speed *s)
 	reg_clkctl |= s->acpu_src_sel << (4 + 8 * src_sel);
 	reg_clkctl |= s->acpu_src_div << (0 + 8 * src_sel);
 	writel(reg_clkctl, SCSS_CLK_CTL_ADDR);
+
+	/* Program PLL2 L val for overclocked speeds. */
+	if(s->src == PLL_2) {
+		writel(s->acpu_clk_khz/19200, PLL2_L_VAL_ADDR);
+	}
 
 	/* Toggle clock source. */
 	reg_clksel ^= 1;
@@ -350,7 +377,11 @@ static unsigned int acpuclk_get_current_vdd(void)
 	unsigned int vdd_mv;
 
 	vdd_raw = msm_spm_get_vdd();
-	for (vdd_mv = 750; vdd_mv <= 1350; vdd_mv += 25)
+#ifndef CONFIG_JESUS_PHONE
+	for (vdd_mv = 850; vdd_mv <= 1300; vdd_mv += 25)
+#else
+	for (vdd_mv = 850; vdd_mv <= 1450; vdd_mv += 25)
+#endif
 		if (VDD_RAW(vdd_mv) == vdd_raw)
 			break;
 
@@ -373,7 +404,11 @@ static int acpuclk_update_freq_tbl(unsigned int acpu_khz, unsigned int acpu_vdd)
 		pr_err("%s: acpuclk invalid speed %d\n", __func__, acpu_khz);
 		return -1;
 	}
-	if (acpu_vdd > 1350 || acpu_vdd < 750) {
+#ifndef CONFIG_JESUS_PHONE
+	if (acpu_vdd > 1300 || acpu_vdd < 850) {
+#else
+	if (acpu_vdd > 1450 || acpu_vdd < 850) {
+#endif
 		pr_err("%s: acpuclk vdd out of ranage, %d\n",
 			__func__, acpu_vdd);
 		return -2;
@@ -486,18 +521,6 @@ static void __init lpj_init(void)
 	}
 }
 
-/* Update frequency tables for a 1024MHz PLL2. */
-void __init pll2_1024mhz_fixup(void)
-{
-	if (acpu_freq_tbl[ARRAY_SIZE(acpu_freq_tbl)-2].acpu_clk_khz != 806400
-		  || freq_table[ARRAY_SIZE(freq_table)-2].frequency != 806400) {
-		pr_err("Frequency table fixups for PLL2 rate failed.\n");
-		BUG();
-	}
-	acpu_freq_tbl[ARRAY_SIZE(acpu_freq_tbl)-2].acpu_clk_khz = 1024000;
-	freq_table[ARRAY_SIZE(freq_table)-2].frequency = 1024000;
-}
-
 #define RPM_BYPASS_MASK	(1 << 3)
 #define PMIC_MODE_MASK	(1 << 4)
 void __init msm_acpu_clock_init(struct msm_acpu_clock_platform_data *clkdata)
@@ -507,15 +530,14 @@ void __init msm_acpu_clock_init(struct msm_acpu_clock_platform_data *clkdata)
 	mutex_init(&drv_state.lock);
 	drv_state.acpu_switch_time_us = clkdata->acpu_switch_time_us;
 	drv_state.vdd_switch_time_us = clkdata->vdd_switch_time_us;
+	drv_state.power_collapse_khz = clkdata->power_collapse_khz;
 	drv_state.wfi_ramp_down = 1;
 	drv_state.pwrc_ramp_down = 1;
-	/* PLL2 runs at 1024MHz for MSM8x55. */
-	if (cpu_is_msm8x55())
-		pll2_1024mhz_fixup();
 	acpuclk_init();
 	lpj_init();
 
 	cpufreq_frequency_table_get_attr(freq_table, smp_processor_id());
 	register_acpuclock_debug_dev(&acpu_debug_7x30);
 }
+
 
